983ec8fba860e16110c69bf82dad68ebea4d07de
Nabeel Shafiq <engr.nabeelshafiq@gmail.com>
2023-09-23 11:22:36 -0230

    Address feedback
    - Move helper method in views helper instead of model
    - Add letter opener in development environment
    - Remove extra files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  USER_CATEGORY = [
-    ["manager", "Manager"],
-    ["developer", "Developer"],
-    ["qa", "QA"],
-  ]
-
-  enum user_category: {
-    manager: 0,
-    developer: 1,
-    qa: 2
-  }
+  USER_CATEGORIES = { manager: 0, developer: 1, qa: 2 }.freeze
+  
+  enum user_category: USER_CATEGORIES
 end
